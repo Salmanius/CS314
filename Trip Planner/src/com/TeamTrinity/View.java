8080feadabe71dac5ad62e2aa0bc09f4ab269541
mjdunn7 <mjdunn7@yahoo.com>
2017-02-13 21:48:06 -0700

    MVP Skeleton Code Added

--- Trip Planner/src/com/TeamTrinity/View.java
+++ Trip Planner/src/com/TeamTrinity/View.java
@@ -4,4 +4,9 @@ package com.TeamTrinity;
  * Created by mjdun on 2/13/2017.
  */
 public class View {
+    public View() {
+    }
+    public void display(String text) {
+        System.out.println(text);
+    }
 }
