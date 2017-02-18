094d719cc0ae8b94ccd419db5109b33bb0288d8e
Chris Cochran <masterchiefjohn@gmail.com>
2017-02-17 23:50:01 -0700

    Forgot the getters for the Coordinate class.

--- Trip Planner/src/TripCo/Coordinates.java
+++ Trip Planner/src/TripCo/Coordinates.java
@@ -8,10 +8,28 @@ public class Coordinates {
     private int seconds;
     private char direction;
 
+
     public Coordinates(int d, int m, int s, char dir){
         this.degrees = d;
         this.minutes = m;
         this.seconds = s;
         this.direction = dir;
     }
+    
+    public int getDegrees() {
+        return degrees;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
 }
