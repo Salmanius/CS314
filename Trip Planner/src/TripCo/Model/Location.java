33c7bf4fffceec67ef1328960c914c0c855d95f5
mjdunn7 <mjdunn7@yahoo.com>
2017-02-19 18:26:33 -0700

    Added public methods to Location class
    Added public methods to Location class to get started on issue on issue
    #53 and enable work to start on issue #54.

--- Trip Planner/src/TripCo/Model/Location.java
+++ Trip Planner/src/TripCo/Model/Location.java
@@ -6,14 +6,46 @@ import TripCo.Coordinates;
  * Created by mjdun on 2/19/2017.
  */
 public class Location {
-    public Location(String ID, String name, String latitude, String longitude){
+    protected String name;
+    protected String stringLatitude;
+    protected String stringLongitude;
+    protected double dblLatitude;
+    protected double dblLongitude;
 
+    public Location(String ID, String name, String latitude, String longitude){
+        this.name = name;
+        this.stringLatitude = latitude;
+        this.stringLongitude = longitude;
+
+        parseLatitude();
+        parseLongitude();
     }
 
+    public double getIntLatitude(){
+        return dblLatitude;
+    }
 
-    protected String name;
-    protected String city;
-    protected Coordinates latitude;
-    protected Coordinates longitude;
-    protected int altitude;
+    public double getIntLongitude(){
+        return dblLongitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStringLatitude() {
+        return stringLatitude;
+    }
+
+    public String getStringLongitude() {
+        return stringLongitude;
+    }
+
+    private void parseLongitude(){
+        //convert string longitude to double representation
+    }
+
+    private void parseLatitude(){
+        //convert string latitude to double representation
+    }
 }
