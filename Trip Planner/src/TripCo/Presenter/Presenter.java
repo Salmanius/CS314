696ab9a3e76c009c0524c920981022f0d4aea9d3
mjdunn7 <mjdunn7@yahoo.com>
2017-02-21 02:34:32 -0700

    Wrote Presenter code, added some getters

--- Trip Planner/src/TripCo/Presenter/Presenter.java
+++ Trip Planner/src/TripCo/Presenter/Presenter.java
@@ -1,6 +1,8 @@
 package TripCo.Presenter;
 
+import TripCo.Model.Leg;
 import TripCo.Model.Model;
+import TripCo.Model.Trip;
 import TripCo.View.View;
 
 /**
@@ -9,13 +11,23 @@ import TripCo.View.View;
 public class Presenter {
     private View view;
     private Model model;
+
     public Presenter(View view, Model model) {
         this.view = view;
         this.model = model;
     }
     public void start() {
-        String result = model.getMessage();
-        view.display (result);
+        String fileName = view.getFilename();
+        System.out.println(fileName);
+        model.parseCSV(fileName);
+        Trip trip = model.getTrip();
+
+        for(int i = 0; i < trip.amountOfLegs(); ++i){
+            Leg leg = trip.getLeg(i);
+            view.addLocation(leg.getStartLocation().getName(), leg.getStartLocation().getId() ,
+                    leg.getStartLocation().getDblLatitude(), leg.getStartLocation().getDblLongitude(), leg.getMileage());
+            view.printFiles();
+        }
     }
 }
 
