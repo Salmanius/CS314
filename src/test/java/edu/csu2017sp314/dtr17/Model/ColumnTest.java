844e5f04454c974af928cccd334cc0b3acaced6d
Jacob Muzzy <jrmuzzy1@rams.colostate.edu>
2017-03-20 11:29:34 -0600

    making copy of main and test files to put into correct directories

---
+++ src/test/java/edu/csu2017sp314/dtr17/Model/ColumnTest.java
@@ -0,0 +1,50 @@
+package TripCo.Model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mjdun on 2/20/2017.
+ */
+public class ColumnTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void add() throws Exception {
+        Column test = new Column("name");
+        test.add("matt");
+
+        assertEquals("matt", test.getDataAt(0));
+    }
+
+    @Test
+    public void getDataAt() throws Exception {
+        Column test = new Column("name");
+        test.add("matt");
+
+        assertEquals("matt", test.getDataAt(0));
+    }
+
+    @Test
+    public void getColumnName() throws Exception {
+        Column test = new Column("id");
+
+        assertEquals("id", test.getColumnName());
+    }
+
+    @Test
+    public void getHeight() throws Exception {
+        Column test = new Column("name");
+        test.add("matt");
+        test.add("chris");
+        test.add("ben");
+
+        assertEquals(3, test.getHeight());
+    }
+
+}
