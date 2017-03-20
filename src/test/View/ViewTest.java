0d602c18549f7cfd1059eada28c91b525fbe5fb7
Salmanius <masterchiefjohn@gmail.com>
2017-03-19 19:20:18 -0600

    Restructuring to hope Travis will work.
    Moving test files into their own files.

---
+++ src/test/View/ViewTest.java
@@ -0,0 +1,60 @@
+package TripCo.View;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ViewTest {
+    @Test
+    public void getMileageFlag() throws Exception {
+        String[] testArgs = {"test.java", "-m", "input.csv"};
+        View test = new View(testArgs);
+        assertTrue(test.getMileageFlag());
+    }
+
+    @Test
+    public void getIDFlag() throws Exception {
+        String[] testArgs = {"test.java", "-i", "input.csv"};
+        View test = new View(testArgs);
+        assertTrue(test.getIDFlag());
+    }
+
+    @Test
+    public void getNameFlag() throws Exception {
+        String[] testArgs = {"test.java", "-n", "input.csv"};
+        View test = new View(testArgs);
+        assertTrue(test.getNameFlag());
+    }
+
+    @Test
+    public void getFilename() throws Exception {
+        String[] testArgs = {"test.java", "input.csv"};
+        View test = new View(testArgs);
+        assertEquals(test.getFilename(), "input.csv");
+    }
+
+    @Test
+    public void addLocation() throws Exception {
+        String[] testArgs = {"test.java", "-mi", "input.csv"};
+        View test = new View(testArgs);
+        test.addLocation("Fort Collins", "970", 105.0844, 40.5853, 20);
+    }
+
+    @Test
+    public void printFiles() throws Exception {
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
