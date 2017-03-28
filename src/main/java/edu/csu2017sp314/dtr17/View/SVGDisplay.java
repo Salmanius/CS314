e52f7279ba2e7eea90930971bb3a93805bda5bd8
Chris Cochran <Salmanius@users.noreply.github.com>
2017-03-28 00:45:23 -0600

    Update SVGDisplay.java

--- src/main/java/edu/csu2017sp314/dtr17/View/SVGDisplay.java
+++ src/main/java/edu/csu2017sp314/dtr17/View/SVGDisplay.java
@@ -25,8 +25,7 @@ public class SVGDisplay {
         // Display the frame.
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-                System.out.println("SVGDISPLAY.JAVA system.exit CALLED");
-                //System.exit(0);
+                System.exit(0);
             }
         });
         frame.setSize(400, 400);
