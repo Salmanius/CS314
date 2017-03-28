77416b1f93c3d84004068239dff7f986fd80bf16
Salmanius <masterchiefjohn@gmail.com>
2017-03-28 01:02:02 -0600

    Fixed issues with Viewtest errors. Should help fix travis.

--- src/test/java/edu/csu2017sp314/dtr17/View/ViewTest.java
+++ src/test/java/edu/csu2017sp314/dtr17/View/ViewTest.java
@@ -10,35 +10,35 @@ import main.java.edu.csu2017sp314.dtr17.View.*;
 public class ViewTest {
     @Test
     public void getMileageFlag() throws Exception {
-        String[] testArgs = {"test.java", "-m", "input.csv"};
+        String[] testArgs = {"test.java", "-p input.csv", "-m", "input.csv"};
         View test = new View(testArgs);
         assertTrue(test.getMileageFlag());
     }
 
     @Test
     public void getIDFlag() throws Exception {
-        String[] testArgs = {"test.java", "-i", "input.csv"};
+        String[] testArgs = {"test.java", "-p input.csv", "-i", "input.csv"};
         View test = new View(testArgs);
         assertTrue(test.getIDFlag());
     }
 
     @Test
     public void getNameFlag() throws Exception {
-        String[] testArgs = {"test.java", "-n", "input.csv"};
+        String[] testArgs = {"test.java", "-p input.csv", "-n", "input.csv"};
         View test = new View(testArgs);
         assertTrue(test.getNameFlag());
     }
 
     @Test
     public void getFilename() throws Exception {
-        String[] testArgs = {"test.java", "input.csv"};
+        String[] testArgs = {"test.java", "-p input.csv", "input.csv"};
         View test = new View(testArgs);
-        assertEquals(test.getFilename(), "input.csv");
+        assertEquals(test.getFilename(), " input.csv");
     }
 
     @Test
     public void addLocation() throws Exception {
-        String[] testArgs = {"test.java", "-mi", "input.csv"};
+        String[] testArgs = {"test.java", "-mi", "-p input.csv", "input.csv"};
         View test = new View(testArgs);
         test.addLocation("Fort Collins", "970", 105.0844, 40.5853, 20);
     }
