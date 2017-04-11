040caa3b0e061632bad0a55827d5ce8c1bdc0828
mjdunn7 <mjdunn7@yahoo.com>
2017-04-10 20:52:38 -0600

    Search field comboBoxes now initialized with DB
    As per issue issue #188.
    
    Code will also handle clearing the appropriate comboBoxes when metadata
    is changed.

--- src/main/java/edu/csu2017sp314/dtr17/Model/DatabaseFetcher.java
+++ src/main/java/edu/csu2017sp314/dtr17/Model/DatabaseFetcher.java
@@ -1,8 +1,201 @@
 package main.java.edu.csu2017sp314.dtr17.Model;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by maste on 4/7/2017.
  */
 public class DatabaseFetcher {
+    private static final String DB_URL = "jdbc:mysql://faure.cs.colostate.edu/cs314?serverTimezone=MST";
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
 
+    //Definitely the most secure way to store user names and passwords, trust me.
+    private static final String USER_NAME = "mjdunn";
+    private static final String PASSWORD = "830602242";
+
+    protected Connection connection;
+    protected Statement statement;
+
+    public static void main(String[] args) throws SQLException {
+        DatabaseFetcher test = new DatabaseFetcher();
+        test.getAllCountriesInContinent("North America");
+    }
+
+    public DatabaseFetcher(){
+        connect();
+    }
+
+    protected void connect() {
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            System.err.printf("Exception: ");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    protected void disconnect() throws SQLException {
+        connection.close();
+        statement.close();
+    }
+
+    protected void selectFrom(String column, String table, ResultSet resultSet) throws SQLException {
+        connect();
+
+        String query = "select " + column + " from " + table;
+        resultSet = statement.executeQuery(query);
+
+        disconnect();
+    }
+
+    public ArrayList<String> getAllCountriesInContinent(String continent) {
+        ArrayList<String> countries = new ArrayList<String>();
+
+        ResultSet rs = null;
+
+        try {
+            String query = "select name from countries where continent = '" + continent + "'" ;
+
+            connect();
+            rs = statement.executeQuery(query);
+
+
+            while (rs.next()) {
+                countries.add(rs.getString("name"));
+            }
+
+            Collections.sort(countries.subList(1, countries.size()));
+
+            rs.close();
+            disconnect();
+
+        } catch (SQLException e) {
+            System.err.printf("Error in getAllCountriesInContinent: ");
+            System.err.println(e.getMessage());
+        }
+
+        return countries;
+    }
+
+    public ArrayList<String> getAllRegionsInISOCountry(String isoCountry){
+        ArrayList<String> regions = new ArrayList<String>();
+
+        ResultSet rs = null;
+
+        String query = "select name from regions where iso_country = '" + isoCountry + "'" ;
+        try {
+            connect();
+            rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+                regions.add(rs.getString("name"));
+            }
+
+            Collections.sort(regions.subList(1, regions.size()));
+
+            disconnect();
+            rs.close();
+
+        } catch (SQLException e) {
+            System.err.printf("Error in getAllRegionsInISOCountry: ");
+            System.err.println(e.getMessage());
+        }
+
+        return regions;
+    }
+
+    public ArrayList<String> getAllMunicipalitiesInISORegion(String iso_region){
+        ArrayList<String> municipalities = new ArrayList<String>();
+
+        ResultSet rs = null;
+
+        String query = "select distinct municipality from airports where iso_region = '" + iso_region + "'";
+        try {
+            connect();
+            rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+                municipalities.add(rs.getString("municipality"));
+            }
+
+            Collections.sort(municipalities.subList(0, municipalities.size()));
+
+            disconnect();
+            rs.close();
+        } catch (SQLException e) {
+            System.err.printf("Error in getAllMunicipalitiesInRegion: ");
+            System.err.println(e.getMessage());
+        }
+
+        return municipalities;
+    }
+
+    public String getContinentCodeFromName(String continentName){
+        ResultSet rs = null;
+        String id = "";
+
+        String query = "select id from continents where name = '" + continentName + "'";
+        try {
+            connect();
+            rs = statement.executeQuery(query);
+
+            rs.next();
+            id = rs.getString("id");
+
+            disconnect();
+            rs.close();
+        } catch (SQLException e) {
+            System.err.printf("Error in getContinentCodeFromName: ");
+            System.err.println(e.getMessage());
+        }
+
+        return id;
+    }
+
+    public String getCountryCodeFromName(String countryName){
+        ResultSet rs = null;
+        String code = "";
+
+        String query = "select code from countries where name = '" + countryName + "'";
+        try {
+            connect();
+            rs = statement.executeQuery(query);
+
+            rs.next();
+            code = rs.getString("code");
+
+            disconnect();
+            rs.close();
+        } catch (SQLException e) {
+            System.err.printf("error in getCountryCodeFromName:");
+            System.err.println(e.getMessage());
+        }
+
+        return code;
+    }
+
+    public String getISORegionFromName(String regionName){
+        ResultSet rs = null;
+        String code = "";
+
+        String query = "select code from regions where name = '" + regionName + "'";
+        try {
+            connect();
+            rs = statement.executeQuery(query);
+
+            rs.next();
+            code = rs.getString("code");
+
+            disconnect();
+            rs.close();
+        } catch (SQLException e) {
+            System.err.printf("error in getISORegionFromName: ");
+            System.err.println(e.getMessage());
+        }
+
+        return code;
+    }
 }
--- src/main/java/edu/csu2017sp314/dtr17/View/GUIController.java
+++ src/main/java/edu/csu2017sp314/dtr17/View/GUIController.java
@@ -3,20 +3,27 @@ package main.java.edu.csu2017sp314.dtr17.View;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.web.WebView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import main.java.edu.csu2017sp314.dtr17.Model.DatabaseFetcher;
 import main.java.edu.csu2017sp314.dtr17.Presenter.Presenter;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.List;
 
 /**
  * Created by mjdun on 4/1/2017.
  */
 public class GUIController {
     public WebView SVGWebView;
+
     public ComboBox airportTypePicker;
     public ComboBox continentPicker;
     public ComboBox countryPicker;
     public ComboBox regionPicker;
+    public ComboBox municipalityPicker;
+
     public TextField airportNameSearchBox;
     public Button searchButton;
     public ListView selectionListBox;
@@ -25,6 +32,7 @@ public class GUIController {
     public Button clearButton;
     public Button loadButton;
     public Button saveButton;
+
     public ComboBox unitPicker;
     public ComboBox optimizationPicker;
     public CheckBox idCheckBox;
@@ -32,10 +40,12 @@ public class GUIController {
     public CheckBox namesCheckBox;
     public Button showMapButton;
 
+
     protected Presenter presenter;
+    protected DatabaseFetcher fetcher;
 
     public GUIController(){
-        System.out.println("controller constructor called");
+        fetcher = new DatabaseFetcher();
     }
 
     public void displaySVG(String filePath) {
@@ -60,18 +70,65 @@ public class GUIController {
     }
 
     public void continentChanged(ActionEvent actionEvent) {
+        if(!continentPicker.getSelectionModel().isEmpty()) {
+            String value = continentPicker.getValue().toString();
+
+            List<String> countries = fetcher.getAllCountriesInContinent(fetcher.getContinentCodeFromName(value));
+
+            resetComboBox(countryPicker);
+            resetComboBox(regionPicker);
+            resetComboBox(municipalityPicker);
+            countryPicker.getItems().addAll(countries);
+            countryPicker.setDisable(false);
+        }
+    }
+
+    protected void resetComboBox(ComboBox comboBox){
+        if(!comboBox.getSelectionModel().isEmpty()) {
+            comboBox.getSelectionModel().clearSelection();
+        }
+        comboBox.getItems().clear();
+        comboBox.setDisable(true);
+    }
+
+    protected void resetSearchFields(){
+        continentPicker.getSelectionModel().clearSelection();
+
+        resetComboBox(countryPicker);
+        resetComboBox(regionPicker);
+        resetComboBox(municipalityPicker);
+
     }
 
     public void countryChanged(ActionEvent actionEvent) {
+        if(!countryPicker.getSelectionModel().isEmpty()) {
+            String value = countryPicker.getValue().toString();
+
+            List<String> regions = fetcher.getAllRegionsInISOCountry(fetcher.getCountryCodeFromName(value));
+
+            resetComboBox(regionPicker);
+            regionPicker.getItems().addAll(regions);
+            regionPicker.setDisable(false);
+        }
     }
 
     public void regionChanged(ActionEvent actionEvent) {
+        if(!regionPicker.getSelectionModel().isEmpty()) {
+            String value = regionPicker.getValue().toString();
+
+            List<String> municipalities = fetcher.getAllMunicipalitiesInISORegion(fetcher.getISORegionFromName(value));
+
+            resetComboBox(municipalityPicker);
+            municipalityPicker.getItems().addAll(municipalities);
+            municipalityPicker.setDisable(false);
+        }
     }
 
     public void municipalityChanged(ActionEvent actionEvent) {
     }
 
     public void searchButtonPressed(ActionEvent actionEvent) {
+        //resetSearchFields();
     }
 
     public void selectButtonPressed(ActionEvent actionEvent) {
@@ -81,6 +138,13 @@ public class GUIController {
     }
 
     public void loadButtonPressed(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Load Trip");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("csv", "*.csv"));
+
+        File file = fileChooser.showOpenDialog(new Stage());
+
     }
 
     public void saveButtonPressed(ActionEvent actionEvent) {
