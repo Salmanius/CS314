dd6c89714a9524f10fa4723db547de7c04632159
Benascor <Peryton272@gmail.com>
2017-04-11 09:27:35 -0600

    XML Parser
    Added the XML Parser to the Model. I think it'll work.

--- src/main/java/edu/csu2017sp314/dtr17/Model/Model.java
+++ src/main/java/edu/csu2017sp314/dtr17/Model/Model.java
@@ -1,5 +1,9 @@
 package main.java.edu.csu2017sp314.dtr17.Model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -31,6 +35,37 @@ public class Model {
             locations.add(new Location(id, name, latitude, longitude));
         }
 
+    }
+
+    public ArrayList<String> parseXML(String fileName){
+        File file = new File(fileName);
+        String idString = "<id>";
+        String endString = "</id>";
+        ArrayList<String> IDs = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line = br.readLine();
+            if(line != null){
+                if(line.toLowerCase().contains(idString.toLowerCase())){
+                    String result = line.toLowerCase().substring(line.toLowerCase().indexOf(idString.toLowerCase()) + 1, line.toLowerCase().indexOf(endString.toLowerCase()));
+                    IDs.add(result);
+                }
+            }
+
+            while ((line = br.readLine()) != null) {
+                if(line.toLowerCase().contains(idString.toLowerCase())){
+                    String result = line.toLowerCase().substring(line.toLowerCase().indexOf(idString.toLowerCase()) + 1, line.toLowerCase().indexOf(endString.toLowerCase()));
+                    IDs.add(result);
+                }
+            }
+
+            br.close();
+            return IDs;
+
+        } catch(IOException exception){
+            System.out.println("Could not open file: " + fileName);
+            return null;
+        }
     }
 
     public Trip getTrip(boolean twoOptBool, boolean threeOptBool, boolean unit){
